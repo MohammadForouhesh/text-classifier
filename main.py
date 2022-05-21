257d7323ae3b044842c13cb823b53ec3b0b53876
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-05-22 01:27:29 +0430

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ def inference_pipeline(model_path: str, input_text: str):
 def main(dataframe: pd.DataFrame, save_path: str):
     dataframe.replace('', float('NaN')).dropna(inplace=True)
     X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(dataframe.text, dataframe.label, stratify=dataframe.label)
+    X_train, X_eval, y_train, y_eval = sklearn.model_selection.train_test_split(X_train, y_train, stratify=y_train)
     xgb = XgbClf(text_array=dataframe.text)
-    xgb.fit(X_train, y_train)
+    xgb.fit(X_train, y_train, X_eval, y_eval)
     xgb.predict(X_test, y_test)
     xgb.save_model(save_path)
 
 
 if __name__ == '__main__':
     df = pd.read_csv('RelevantTag.csv')
-    df.rename(columns={'content': 'text', 'Irelevant': 'label'})
+    df.rename(columns={'content': 'text', 'Irelevant': 'label'}, inplace=True)
     main(df, 'arnika')
     # print(df.text[3])
     # xgb = XgbClf(text_array=None, labels=None, load_path='politics')
